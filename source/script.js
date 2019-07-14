6988e9d6ff200f56573b16610c093f4eddaa6f36
Aryan Gupta <aryan@Aryans-MacBook-Air.local>
2019-07-15 04:45:30 +0530

    completed work expansion panel

--- source/script.js
+++ source/script.js
@@ -9,6 +9,13 @@ $(document).ready(function() {
   	let endMonth = $(this).find('.workDurationEndMonth').html();
   	let endYear = $(this).find('.workDurationEndYear').html();
   	let description = $(this).find('.workDescription').html();
+    let imgNum = $(this).find('.workImagesContainer').find('div').length;
+    let imgArr = [];
+    var i;
+    for (i=0; i < imgNum; i++) {
+      imgArr.push($(this).find('.workImagesContainer').find('div:eq(' + i + ')').css('background-image'));
+    }
+    var imgTracker = 0;
   	$('.bodyOverlay').css({
   		'display': 'flex',
   	})
@@ -18,5 +25,19 @@ $(document).ready(function() {
   	$('.workLoaderEndMonth').html(endMonth);
   	$('.workLoaderEndYear').html(endYear);
   	$('.workLoaderDescription').html(description);
+    $('.workLoaderImg').css('background-image', imgArr[imgTracker]);
+    $('.arrowLeft').click(function(){
+      imgTracker = ((((imgTracker - 1) % imgNum) + imgNum) % imgNum) ;
+      $('.workLoaderImg').css('background-image', imgArr[imgTracker]);
+    })
+    $('.arrowRight').click(function(){
+      imgTracker = (imgTracker + 1) % imgNum;
+      $('.workLoaderImg').css('background-image', imgArr[imgTracker]);
+    })
   })
+  $('.workLoaderCloser').click(function(){
+    $('.bodyOverlay').css({
+      'display': 'none',
+    })
+  });
 })
