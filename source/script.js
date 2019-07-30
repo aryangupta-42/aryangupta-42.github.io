db5c4feb86b9e053256d5c3b40fcd91067c14343
Aryan Gupta <aryan@Aryans-MacBook-Air.local>
2019-07-30 17:43:05 +0530

    recreated work expansion panel image handeling

--- source/script.js
+++ source/script.js
@@ -95,36 +95,39 @@ $(document).ready(function() {
   	let endMonth = $(this).find('.workDurationEndMonth').html();
   	let endYear = $(this).find('.workDurationEndYear').html();
   	let description = $(this).find('.workDescription').html();
-    let imgNum = $(this).find('.workImagesContainer').find('div').length;
+    let imgNum = $(this).find('.workImagesContainer').find('img').length;
     let imgArr = [];
     var i;
     for (i=0; i < imgNum; i++) {
-      imgArr.push($(this).find('.workImagesContainer').find('div:eq(' + i + ')').css('background-image'));
+      imgArr.push($(this).find('.workImagesContainer').find('img:eq(' + i + ')').attr('src'));
     }
+    // alert(imgArr[2].attr('src'));
     var imgTracker = 0;
   	$('.bodyOverlay').css({
   		'display': 'flex',
   	})
+    $('.workLoader').css('height', ($('.workLoaderContainer').height()*95)/100 + 'px');
   	$('.workLoaderTitle').html(title);
   	$('.workLoaderStartMonth').html(startMonth);
   	$('.workLoaderStartYear').html(startYear);
   	$('.workLoaderEndMonth').html(endMonth);
   	$('.workLoaderEndYear').html(endYear);
   	$('.workLoaderDescription').html(description);
-    $('.workLoaderImg').css('background-image', imgArr[imgTracker]);
+    $('.workLoaderImg').find('img').attr('src', imgArr[imgTracker]);
     $('.arrowLeft').click(function(){
       imgTracker = ((((imgTracker - 1) % imgNum) + imgNum) % imgNum) ;
-      $('.workLoaderImg').css('background-image', imgArr[imgTracker]);
+      $('.workLoaderImg').find('img').attr('src', imgArr[imgTracker]);
     })
     $('.arrowRight').click(function(){
       imgTracker = (imgTracker + 1) % imgNum;
-      $('.workLoaderImg').css('background-image', imgArr[imgTracker]);
+      $('.workLoaderImg').find('img').attr('src', imgArr[imgTracker]);
     })
   })
   $('.workLoaderCloser').click(function(){
     $('.bodyOverlay').css({
       'display': 'none',
     })
+    $('.workLoaderImg').find('img').attr('src', "");
   });
 
   $('.navbarHome').click(function(){
