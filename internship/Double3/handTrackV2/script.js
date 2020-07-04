239a5bac7a5f11c5182f9bc37b013ab2dfccdd4f
Aryan Gupta <aryanguptaleo@gmail.com>
2020-07-04 17:43:09 +0530

    Added quick action support on handtrackV2

--- internship/Double3/handTrackV2/script.js
+++ internship/Double3/handTrackV2/script.js
@@ -125,6 +125,24 @@ const gestureModel = {
     // process: false // to be used to ensure that the robot command has been performed completly
 };
 
+// SET HTML HELPER MESSAGES FOR THE USER
+
+const setGeneralInfo = (message) => {
+    $('.commandInfoContainer .generalInfo span').html(message);
+};
+const setHandStatus = (message) => {
+    $('.commandInfoContainer .handStatus span').html(message);
+};
+const setGestureStatus = (message) => {
+    $('.commandInfoContainer .gestureStatus span').html(message);
+};
+
+const setHandGesture = (message) => {
+    $('.commandInfoContainer .handGesture span').html(message);
+};
+
+// HELPER FUNCTIONS FOR GESTURE ANALYSIS
+
 const checkFingerWrap = (fingerAnnotation) => {
     if (fingerAnnotation[1][1] < fingerAnnotation[3][1]) {
         return true;
@@ -186,8 +204,111 @@ const getHandCenter = (annotations) => {
     return { x: xAvg, y: yAvg };
 };
 
-const minGestureTime = 5;
+// setting up gesture parameters
+const minGestureTime = 3;
+const xGradLim = 150;
+const yGradLim = 150;
 
+const checkDirectionalMovement = (del, grad) => {
+    if (Math.abs(del) < grad) {
+        return false;
+    }
+    return true;
+};
+// --------------- GESTURE ANALYSIS ----------------------
+
+// QUICK GESTURE ANALYSIS
+
+const fingerUp = (annotations, fingerNum, single = false) => {
+    // only check for any three fingers at a time...if I take 4 fingers, then that's same as hand open
+    const { indexFinger, ringFinger, middleFinger, pinky } = annotations;
+    let fingers = [indexFinger, middleFinger, ringFinger, pinky];
+    let gesture = true;
+    if (!single) {
+        if (checkFingerWrap(fingers[fingerNum])) {
+            gesture = false;
+        }
+    } else {
+        // ========== TO CHECK FOR ONLY THAT ONE PARTICUALR FINGER UP ==================
+        fingers.forEach((finger, index) => {
+            if (index === fingerNum) {
+                if (checkFingerWrap(finger)) {
+                    gesture = false;
+                }
+            } else {
+                if (!checkFingerWrap(finger)) {
+                    gesture = false;
+                }
+            }
+        });
+    }
+    return gesture;
+};
+
+// ANALYZE SWIPE GESTURES
+const analyzeGesture = () => {
+    // Some reference data
+    // left swipe => delx = -ve
+    // xfin: 533.9469011428394
+    // xinit: 77.36991711257636
+    // yfin: 264.1426477855821
+    // yinit: 234.3790196198422
+
+    // down swipe => dely = -ve
+
+    // xfin: 219.31797668105185
+    // xinit: 230.78144968106326
+    // yfin: 373.4764048806069
+    // yinit: 76.10286491875027
+
+    const { xinit, xfin, yinit, yfin } = gestureModel;
+    // if positive then it's normal coordinate direction
+    // we take init - fin as the picture, and hence the coordinates are inverted
+    const delx = xinit - xfin;
+    const dely = yinit - yfin;
+    const movx = checkDirectionalMovement(delx, xGradLim);
+    const movy = checkDirectionalMovement(dely, yGradLim);
+
+    let gestureResult = '';
+    if (!movx && !movy) {
+        gestureResult =
+            'Please make longer swipes, this one was too short to be recognised';
+    } else if (!movx) {
+        if (dely > 0) {
+            gestureResult = 'You performed an up swipe';
+        } else {
+            gestureResult = 'You performed a down swipe';
+        }
+    } else if (!movy) {
+        if (delx > 0) {
+            gestureResult = 'You performed a right swipe';
+        } else {
+            gestureResult = 'You performed a left swipe';
+        }
+    } else {
+        if (delx > 0 && dely > 0) {
+            gestureResult =
+                'You performed a diagnal swipe => from bottom left to top right';
+        } else if (delx > 0 && dely < 0) {
+            gestureResult =
+                'You performed a diagnal swipe => from top left to bottom right';
+        } else if (delx < 0 && dely > 0) {
+            gestureResult =
+                'You performed a diagnal swipe => from bottom right to top left';
+        } else if (delx < 0 && dely < 0) {
+            gestureResult =
+                'You performed a diagnal swipe => from top right to bottom left';
+        }
+    }
+
+    setHandGesture(gestureResult);
+    setGestureStatus('Gesture analyzed');
+    // now correspondingly perform robotic command here;
+    // then i set gestureModel.process to false
+    gestureModel.process = false;
+};
+
+// ACTUAL RENDERING OUT OF THE CANVAS AND HAND WIREFRAME
 const landmarksRealTime = async (video) => {
     videoWidth = video.videoWidth;
     videoHeight = video.videoHeight;
@@ -211,6 +332,7 @@ const landmarksRealTime = async (video) => {
     let timer;
     let timerActive = false;
     let timerCount = 0;
+    let quickGestureType = 0;
     async function frameLandmarks() {
         ctx.drawImage(
             video,
@@ -227,24 +349,27 @@ const landmarksRealTime = async (video) => {
         if (predictions.length > 0) {
             const landmarkResult = predictions[0].landmarks;
             const annotationResult = predictions[0].annotations;
-            const baseCoor = landmarkResult[0];
+            // const baseCoor = landmarkResult[0];
             if (checkHandStatus(annotationResult, 'close')) {
-                $('.commandInfoContainer .handStatus span').html('Closed');
-                if (!gestureModel.active) {
+                setHandStatus('Closed');
+                if (!gestureModel.active && !gestureModel.process) {
                     if (!timerActive) {
                         timerCount = 0;
                         timer = setInterval(() => {
                             timerCount += 1;
-                            $('.commandInfoContainer .generalInfo span').html(
+                            setGeneralInfo(
                                 `Timer started! Keep hand closed for ${
                                     minGestureTime - timerCount
                                 } more seconds to begin gesture tracking or open your hand to stop gesture`
                             );
                         }, 1000);
-                        $('.commandInfoContainer .generalInfo span').html(
+                        setGeneralInfo(
                             'Timer started! Keep hand closed for 5 more seconds to begin gesture tracking or open your hand to stop gesture'
                         );
                         timerActive = true;
+                        setGestureStatus(
+                            'Gesture trying to begin, Timer running'
+                        );
                     } else {
                         if (timerCount > minGestureTime) {
                             clearInterval(timer);
@@ -257,32 +382,42 @@ const landmarksRealTime = async (video) => {
                             gestureModel.yinit = handInitCoor.y;
                         }
                     }
+                } else if (gestureModel.process) {
+                    clearInterval(timer);
+                    timerActive = false;
+                    setGeneralInfo(
+                        'A command is being performed. Please wait for it to finish before making another gesture'
+                    );
                 } else {
                     clearInterval(timer);
                     timerActive = false;
-                    $('.commandInfoContainer .generalInfo span').html(
+                    setGeneralInfo(
                         'Gesture is being tracked, open your hand to finish gesture'
                     );
+                    setGestureStatus('Gesture being tracked');
                 }
             } else if (checkHandStatus(annotationResult, 'open')) {
-                $('.commandInfoContainer .handStatus span').html('Open');
+                setHandStatus('Open');
                 if (gestureModel.active) {
                     if (!timerActive) {
                         timerCount = 0;
                         timer = setInterval(() => {
                             timerCount += 1;
-                            $('.commandInfoContainer .generalInfo span').html(
+                            setGeneralInfo(
                                 `Timer started! Keep hand open for ${
                                     minGestureTime - timerCount
                                 } more seconds to finish gesture or close hand to update gesture tracking`
                             );
                         }, 1000);
-                        $('.commandInfoContainer .generalInfo span').html(
+                        setGeneralInfo(
                             `Timer started! Keep hand open for ${
                                 minGestureTime - timerCount
                             } more seconds to finish gesture or close hand to update gesture tracking`
                         );
                         timerActive = true;
+                        setGestureStatus(
+                            'Gesture trying to finish, timer ongoing'
+                        );
                     } else {
                         if (timerCount > minGestureTime) {
                             clearInterval(timer);
@@ -290,36 +425,152 @@ const landmarksRealTime = async (video) => {
                             const handFinCoor = getHandCenter(annotationResult);
                             gestureModel.xfin = handFinCoor.x;
                             gestureModel.yfin = handFinCoor.y;
-                            console.log(gestureModel);
                             gestureModel.active = false;
+                            gestureModel.process = true;
+                            analyzeGesture();
                         }
                     }
                 } else {
                     clearInterval(timer);
                     timerActive = false;
-                    $('.commandInfoContainer .generalInfo span').html(
+                    setGeneralInfo(
                         'No gesture currently in progress, close your hand to begin gesture tracking'
                     );
+                    setGestureStatus('No gesture in progress');
                 }
             } else {
                 // hand is neither closed nor open;
-                clearInterval(timer);
-                timerActive = false;
-                $('.commandInfoContainer .generalInfo span').html(
-                    'No gesture currently in progress, close your hand to begin gesture tracking'
-                );
+                if (!gestureModel.active) {
+                    if (fingerUp(annotationResult, 0, true)) {
+                        if (quickGestureType === 1) {
+                            if (!timerActive) {
+                                timerCount = 0;
+                                timer = setInterval(() => {
+                                    timerCount += 1;
+                                    setGeneralInfo(
+                                        `Timer started! Keep holding this gesture for ${
+                                            minGestureTime - timerCount
+                                        } more seconds to complete`
+                                    );
+                                }, 1000);
+                                timerActive = true;
+                                setGeneralInfo(
+                                    `Timer started! Keep holding this gesture for ${
+                                        minGestureTime - timerCount
+                                    } more seconds to complete`
+                                );
+                            } else {
+                                if (timerCount > minGestureTime) {
+                                    clearInterval(timer);
+                                    timerActive = false;
+                                    gestureModel.process = true;
+                                    // perform a robot command
+                                    gestureModel.process = false;
+                                    setHandGesture('Quick action 1 performed');
+                                }
+                                setGestureStatus('Index finger up');
+                            }
+                        } else {
+                            quickGestureType = 1;
+                            clearInterval(timer);
+                            timerActive = false;
+                        }
+                    } else if (
+                        fingerUp(annotationResult, 0) &&
+                        fingerUp(annotationResult, 1) &&
+                        !fingerUp(annotationResult, 2)
+                    ) {
+                        if (quickGestureType === 2) {
+                            if (!timerActive) {
+                                timerCount = 0;
+                                timer = setInterval(() => {
+                                    timerCount += 1;
+                                    setGeneralInfo(
+                                        `Timer started! Keep holding this gesture for ${
+                                            minGestureTime - timerCount
+                                        } more seconds to complete`
+                                    );
+                                }, 1000);
+                                timerActive = true;
+                                setGeneralInfo(
+                                    `Timer started! Keep holding this gesture for ${
+                                        minGestureTime - timerCount
+                                    } more seconds to complete`
+                                );
+                            } else {
+                                if (timerCount > minGestureTime) {
+                                    clearInterval(timer);
+                                    timerActive = false;
+                                    gestureModel.process = true;
+                                    // perform a robot command
+                                    gestureModel.process = false;
+                                    setHandGesture('Quick action 2 performed');
+                                }
+                                setGestureStatus('Two fingers up');
+                            }
+                        } else {
+                            quickGestureType = 2;
+                            clearInterval(timer);
+                            timerActive = false;
+                        }
+                    } else if (
+                        fingerUp(annotationResult, 0) &&
+                        fingerUp(annotationResult, 1) &&
+                        fingerUp(annotationResult, 2)
+                    ) {
+                        if (quickGestureType === 3) {
+                            if (!timerActive) {
+                                timerCount = 0;
+                                timer = setInterval(() => {
+                                    timerCount += 1;
+                                    setGeneralInfo(
+                                        `Timer started! Keep holding this gesture for ${
+                                            minGestureTime - timerCount
+                                        } more seconds to complete`
+                                    );
+                                }, 1000);
+                                timerActive = true;
+                                setGeneralInfo(
+                                    `Timer started! Keep holding this gesture for ${
+                                        minGestureTime - timerCount
+                                    } more seconds to complete`
+                                );
+                            } else {
+                                if (timerCount > minGestureTime) {
+                                    clearInterval(timer);
+                                    timerActive = false;
+                                    gestureModel.process = true;
+                                    // perform a robot command
+                                    gestureModel.process = false;
+                                    setHandGesture('Quick action 3 performed');
+                                }
+                                setGestureStatus('Three fingers up');
+                            }
+                        } else {
+                            quickGestureType = 3;
+                            clearInterval(timer);
+                            timerActive = false;
+                        }
+                    } else {
+                        clearInterval(timer);
+                        timerActive = false;
+                        setGestureStatus('Unrecognised gesture');
+                    }
+                } else {
+                    clearInterval(timer);
+                    timerActive = false;
+                    setGeneralInfo('A swipe gesture is currenly being tracked');
+                }
+                setHandStatus('Neither');
             }
             drawKeypoints(ctx, landmarkResult, predictions[0].annotations);
         } else {
             if (!gestureModel.active) {
                 clearInterval(timer);
                 timerActive = false;
-                $('.commandInfoContainer .generalInfo span').html(
-                    'No hand detected'
-                );
-                $('.commandInfoContainer .handStatus span').html(
-                    'No hand detected'
-                );
+                setGeneralInfo('No hand detected');
+                setHandStatus('No hand detected');
+                setGestureStatus('No hand on screen');
             }
         }
         requestAnimationFrame(frameLandmarks);
